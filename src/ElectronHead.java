462e1d63a4892717135f13350f3460ae1e82e12c
Kamil Samul <kamrep7@wp.pl>
2017-04-14 14:54:15 +0200

    Added: ElectronHead and ElectronTail

---
+++ src/ElectronHead.java
@@ -0,0 +1,49 @@
+/**
+ * Created on 14.04.2017 at 14:48 for usage in WireWorld.
+ */
+public class ElectronHead extends WireElement{
+    private int posX;
+    private int posY;
+    private int sizeX = 1;
+    private int sizeY = 1;
+    private int rotation;
+    private byte grid;
+
+    public ElectronHead(int posX, int posY, int rotation){
+        this.posX = posX;
+        this.posY = posY;
+        this.rotation = rotation;
+
+        grid = 2;
+    }
+
+    @Override
+    public int getPosX() {
+        return posX;
+    }
+
+    @Override
+    public int getPosY() {
+        return posY;
+    }
+
+    @Override
+    public int getRotation(){
+        return rotation;
+    }
+
+    @Override
+    public byte getGridPoint(int posX, int posY){
+        return 3;
+    }
+
+    @Override
+    public int getSizeX(){
+        return sizeX;
+    }
+
+    @Override
+    public int getSizeY(){
+        return sizeY;
+    }
+}
---
+++ src/ElectronTail.java
@@ -0,0 +1,49 @@
+/**
+ * Created on 14.04.2017 at 14:51 for usage in WireWorld.
+ */
+public class ElectronTail extends WireElement{
+    private int posX;
+    private int posY;
+    private int sizeX = 1;
+    private int sizeY = 1;
+    private int rotation;
+    private byte grid;
+
+    public ElectronTail(int posX, int posY, int rotation){
+        this.posX = posX;
+        this.posY = posY;
+        this.rotation = rotation;
+
+        grid = 2;
+    }
+
+    @Override
+    public int getPosX() {
+        return posX;
+    }
+
+    @Override
+    public int getPosY() {
+        return posY;
+    }
+
+    @Override
+    public int getRotation(){
+        return rotation;
+    }
+
+    @Override
+    public byte getGridPoint(int posX, int posY){
+        return 2;
+    }
+
+    @Override
+    public int getSizeX(){
+        return sizeX;
+    }
+
+    @Override
+    public int getSizeY(){
+        return sizeY;
+    }
+}
